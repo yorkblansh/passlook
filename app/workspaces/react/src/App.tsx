204c279624a7ae63ac887e868126c2eafe4a9768
Oleg Chepelev <yorkblansh@yandex.com>
2024-07-11 11:05:59 +0300

    react: upd layout

--- app/workspaces/react/src/App.tsx
+++ app/workspaces/react/src/App.tsx
@@ -1,8 +1,38 @@
 import _ from 'lodash'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { SearchInput } from './shared/ui/SearchInput/SearchInput'
+import './App.scss'
+import { SearchSubjectChanger } from './shared/ui/SearchSubjectChanger/SearchSubjectChanger'
+import { SearchBar } from './shared/ui/SearchBar/SearchBar'
+
+const SearchPage = () => {
+	return (
+		<div>
+			ПОИСК
+			<div>по паролю</div>
+			<SearchBar>
+				<SearchInput />
+				<SearchSubjectChanger
+					subjects={[
+						'по паролю',
+						'по email',
+						'по ФИО',
+						'по должности',
+						'по отделу',
+					]}
+					default="по email"
+				/>
+			</SearchBar>
+		</div>
+	)
+}
 
 const App = () => {
-	return <main className="App">123</main>
+	return (
+		<main className="App">
+			<SearchPage />
+		</main>
+	)
 }
 
 export const Router = () => {
